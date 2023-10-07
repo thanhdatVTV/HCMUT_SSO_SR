3310079f990be5c8ad8ca6db5d18976de4bd07b3
thanhdatVTV <106882741+thanhdatVTV@users.noreply.github.com>
2023-10-07 18:12:30 +0700

    API check login

--- HCMUT_SSO/Controllers/UserController.cs
+++ HCMUT_SSO/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using HCMUT_SSO.Models;
+using HCMUT_SSO.ViewModels.ResultView;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,26 @@ namespace HCMUT_SSO.Controllers
         [HttpGet]
         public async Task<ActionResult<List<TblUser>>> Getuser() {
             return Ok(await _context.TblUsers.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("get-by-id")]
+        public async Task<IActionResult> CheckAuthenticationLogin(string userName, string passWord)
+        {
+            ResultViewModel Result = new ResultViewModel();
+
+            var user = await _context.TblUsers.Where(d => d.UserName == userName && d.Password == passWord).FirstOrDefaultAsync();
+            if (user == null)
+            {
+                Result.status = 0;
+                Result.message = "Tài khoản không tồn tại trong hệ thống!";
+            }
+            else
+            {
+                Result.status = 1;
+                Result.message = "Tài khoản chính xác!";
+            }
+            return Ok(Result);
         }
     }
 }
---
+++ HCMUT_SSO/ViewModels/ResultView/ResultViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HCMUT_SSO.ViewModels.ResultView
+{
+    public class ResultViewModel
+    {
+        public ResultViewModel()
+        {
+            status = 1;
+            message = "success";
+
+        }
+
+        public int status { get; set; }
+        public string message { get; set; }
+        public object response { get; set; }
+        public int totalRecord { get; set; }
+    }
+}
